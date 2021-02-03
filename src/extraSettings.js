bf723c91a267c354275cac45c80ad7ef6c3283cf
junjunzi-gif <78468806+junjunzi-gif@users.noreply.github.com>
2021-02-03 22:35:34 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -26,7 +26,7 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.9;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
@@ -38,7 +38,7 @@ let clickChangeFruit = false;
 let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成大学霸';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
